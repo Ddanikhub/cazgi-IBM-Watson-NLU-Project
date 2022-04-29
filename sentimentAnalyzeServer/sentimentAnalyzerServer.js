aa1271f2ffe40b07e68d993ebe80664ab4d511fb
Ddanikhub <ddorohov@gmail.com>
2022-04-28 23:56:18 -0700

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -23,28 +23,28 @@ function getNLUInstance() {
     // /*Type the code to create the NLU instance and return it.
     // You can refer to the image in the instructions document
     // to do the same.*/
-    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-    const { IamAuthenticator } = require('ibm-watson/auth');
-
-    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-        version: '2021-08-01',
-        authenticator: new IamAuthenticator({
-            apikey: api_key
-        }),
-        serviceUrl: api_url
-    });
-    return naturalLanguageUnderstanding;
     // const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
     // const { IamAuthenticator } = require('ibm-watson/auth');
 
     // const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-    //     version: '2022-04-07',
+    //     version: '2021-08-01',
     //     authenticator: new IamAuthenticator({
-    //         apikey: api_key,
+    //         apikey: api_key
     //     }),
-    //     serviceUrl: api_url,
+    //     serviceUrl: api_url
     // });
     // return naturalLanguageUnderstanding;
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2022-04-07',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
 
 }
 
